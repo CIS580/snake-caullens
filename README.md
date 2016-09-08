78455f2d39cb9283bcdb3a93c120080e9da649a9
caullens <caullens@ksu.edu>
2016-09-08 17:12:37 -0500

    Completed assignment

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ The game is implemented in JavaScript using the HTML5 canvas element using the p
 
 1. The snake consists of a head and multiple body segments which always follow behind it and are rendered on-screen. The segments _should not be fixed in position_, i.e. the tail should move around the screen.  The head and body segments can be composed of rectangles, arcs, or images (10 points)
 2. The snake's head can move left, right, up, and down. Optionally you may prevent the snake from turning back upon its own body (10 points)
-3. Moving the snake's head off-screen (out of the rectangle between 0, 0, and the canvas width and height) loses the game (10 points)
+3. Moving the snake's head off-screen (out of the rectangle between 0, 0, and the canvas width and height) loses the game (10 points) !!I purposely chose to make the snake wrap around to the other side because that's how I've always seen Snake work!!
 4. Colliding the snake's head with a body segment loses the game (10 points)
 5. When the game is lost, a message to that effect should be displayed to the players.  This can be done with the canvas' drawText method or an HTML element overlaying the canvas.  It should _not_ be done with the _alert()_ function (10 points)
 6. When the game is lost, the snake should no longer move (10 points)
@@ -19,18 +19,3 @@ The game is implemented in JavaScript using the HTML5 canvas element using the p
 ## Extra Credit
 1. You may optionally implement obstacles within the game's area.  Should the snake collide with one, the game should end. (10)
 2. A bonus of 10 points is available for exceptionally polished game implementations.
-
-Collision info: |line| < r1+r2 CIRLCES
-Distance Formula: d = sqrt((x1-x2)^(2)-(y1-y2)^(2))
-if(line < r1+r2) then collision
-Better to square both sides instead. Avoid sqrt
-
-Collision info: !(Abot > Btop || Atop < Bbot || Aleft > Bright || Aright < Bleft)
-
-Cirlce and Rectangle -> Just do rectangle logic for basic implementation
-
-Making and maintaining the body: var arr = [{cx:0,cy:5,r:5},...];
-arr.push({_}) //Puts element at the back of the array. arr.pop //Remove
-arr.unshift({_}) //Puts element at the front of the array. arr.shift //Remove
-
-Check head verses all other elements in body array.
--- snake.js
+++ snake.js
@@ -6,38 +6,78 @@ backBuffer.width = frontBuffer.width;
 backBuffer.height = frontBuffer.height;
 var backCtx = backBuffer.getContext('2d');
 var oldTime = performance.now();
+var highScore = 0;
 
-var snakeX = new Array();
-var snakeY = new Array();
+var score;
+var snakeX;
+var snakeY;
+var snake;
+var apple;
+var input;
+var prevInput;
 
-var snake = {
-  x: Math.floor(backBuffer.width/2),
-  y: Math.floor(backBuffer.height/2),
-  width: 10,
-  height: 10,
-  speed: 3
+var isPaused;
+
+//All assets I use are imported. All art is my own.
+var bg = new Image();
+bg.src = "assets/background.png";
+
+var appleIm = new Image();
+appleIm.src = "assets/apple.png";
+
+var snakeUp = new Image();
+snakeUp.src = "assets/snake-up.png";
+
+var snakeRight = new Image();
+snakeRight.src = "assets/snake-right.png";
+
+var snakeDown = new Image();
+snakeDown.src = "assets/snake-down.png";
+
+var snakeLeft = new Image();
+snakeLeft.src = "assets/snake-left.png";
+
+//Sets all game variable to initial values
+function init() {
+  snakeX = new Array();
+  snakeY = new Array();
+
+  isPaused = false;
+
+  snake = {
+    x: Math.floor(backBuffer.width/2),
+    y: Math.floor(backBuffer.height/2),
+    width: 10,
+    height: 10,
+    speed: 3,
+    image: snakeUp
+  }
+
+  apple = {
+    x: Math.floor(Math.random()*backBuffer.width),
+    y: Math.floor(Math.random()*backBuffer.height),
+    width: 15,
+    height: 15
+  }
+
+  input = {
+    up: false,
+    down: false,
+    left: false,
+    right: false 
+  }
+
+  prevInput = {
+    up: false,
+    down: false,
+    left: false,
+    right: false
+  }
+
+  score = 0;
 }
 
-var apple = {
-  x: Math.floor(Math.random()*backBuffer.width),
-  y: Math.floor(Math.random()*backBuffer.height),
-  width: 10,
-  height: 10
-}
-
-var input = {
-  up: false,
-  down: false,
-  left: false,
-  right: false 
-}
-
-var prevInput = {
-  up: false,
-  down: false,
-  left: false,
-  right: false
-}
+init();
 
 window.onkeydown = function(event)
 {
@@ -64,7 +104,16 @@ window.onkeydown = function(event)
 		case 68:
 			input.right = true;
 			break;
-		
+    case 80:
+      if(isPaused) isPaused = false;
+      else isPaused = true;
+    //ANY OTHER KEY
+		default:
+      if(prevInput.up) input.up = true;
+      if(prevInput.down) input.down = true;
+      if(prevInput.left) input.left = true;
+      if(prevInput.right) input.right = true;
+      break;
 	}
 }
 
@@ -77,7 +126,7 @@ function loop(newTime) {
   var elapsedTime = newTime - oldTime;
   oldTime = newTime;
 
-  update(elapsedTime);
+if(!isPaused) update(elapsedTime);
   render(elapsedTime);
 
   frontCtx.clearRect(0, 0, frontBuffer.width, frontBuffer.height);
@@ -102,33 +151,44 @@ function update(elapsedTime) {
     snake.y -= snake.speed;
     resetPrevInput();
     prevInput.up = true;
+    snake.image = snakeUp;
   }
   else if(input.down && !prevInput.up) { 
     snake.y += snake.speed;
     resetPrevInput();
     prevInput.down = true;
+    snake.image = snakeDown;
   }
   else if(input.left && !prevInput.right) {
     snake.x -= snake.speed;
     resetPrevInput();
     prevInput.left = true;
+    snake.image = snakeLeft;
   }
   else if(input.right && !prevInput.left) {
     snake.x += snake.speed;
     resetPrevInput();
     prevInput.right = true;
+    snake.image = snakeRight;
   }
-  else if(input.up && prevInput.down) snake.y += snake.speed;
-  else if(input.down && prevInput.up) snake.y -= snake.speed;
-  else if(input.left && prevInput.right) snake.x += snake.speed;
-  else if(input.right && prevInput.left) snake.x -= snake.speed;
+  else if(input.up && prevInput.down) {
+    snake.y += snake.speed;
+    snake.image = snakeDown;
+  }
+  else if(input.down && prevInput.up) {
+    snake.y -= snake.speed;
+    snake.image = snakeUp;
+  }
+  else if(input.left && prevInput.right) {
+    snake.x += snake.speed;
+    snake.image = snakeRight;
+  }
+  else if(input.right && prevInput.left) {
+    snake.x -= snake.speed;
+    snake.image = snakeLeft;
+  }
 
-  // Determine if the snake has moved out-of-bounds (offscreen)
-  if(snake.x >= backBuffer.width) snake.x=1;
-  if(snake.y >= backBuffer.height) snake.y=1;
-  if(snake.x <= 0) snake.x = backBuffer.width;
-  if(snake.y <= 0) snake.y = backBuffer.height;
-
+  //Update the coordinate arrays
   snakeX.unshift(snake.x);
   snakeY.unshift(snake.y);
   if(snakeX.length > 1) {
@@ -136,31 +196,37 @@ function update(elapsedTime) {
     snakeY.pop();
   }
 
+  // Determine if the snake has moved out-of-bounds (offscreen)
+  if(snake.x >= backBuffer.width) snake.x=1;
+  if(snake.y >= backBuffer.height) snake.y=1;
+  if(snake.x <= 0) snake.x = backBuffer.width;
+  if(snake.y <= 0) snake.y = backBuffer.height;
+
   // Determine if the snake has eaten an apple
   if(snake.x < apple.x + apple.width &&
      snake.x + snake.width > apple.x && 
      snake.y < apple.y + apple.height &&
      snake.y + snake.height > apple.y) {
-       apple.x = Math.floor(Math.random()*backBuffer.width);
-       apple.y = Math.floor(Math.random()*backBuffer.height);
-       for(i=0; i<5; i++) {
+       spawnApple();
+       for(i=0; i<10; i++) {
           snakeX.push(snakeX[snakeX.length]);
           snakeY.push(snakeY[snakeY.length]);
        }
+       score += 100;
      }
-
+  
+  //Update highscore
+  if(score > highScore) highScore = score;
+  
   // Determine if the snake has eaten its tail
-  /*for(i=6; i<snakeX.length; i++) {
+  for(i=12; i<snakeX.length; i++) {
       if(snakeX[0] < snakeX[i] + snake.width &&
       snakeX[0] + snake.width > snakeX[i] && 
       snakeY[0] < snakeY[i] + snake.height &&
       snakeY[0] + snake.height > snakeY[i]) {
-          resetInput();
-          resetPrevInput();
+        init();
       }
-  } */
-  // TODO: [Extra Credit] Determine if the snake has run into an obstacle
-
+  }
 }
 
 /**
@@ -173,15 +239,29 @@ function render(elapsedTime) {
   backCtx.clearRect(0, 0, backBuffer.width, backBuffer.height);
 
   // Draw the game objects into the backBuffer
+  backCtx.drawImage(bg,0,0);
   backCtx.fillStyle = "red";
-  backCtx.fillRect(apple.x,apple.y,apple.width,apple.height);
+  backCtx.drawImage(appleIm,apple.x,apple.y,15,15);
 
   backCtx.fillStyle = "green";
-  for(i=0; i<snakeX.length; i++) {
-    backCtx.fillRect(snakeX[i],snakeY[i],apple.width,apple.height);
+  for(i=1; i<snakeX.length; i++) {
+    backCtx.fillRect(snakeX[i],snakeY[i],snake.width,snake.height);
   }
+
+  //Logic for orientation of head
+  if(snake.image == snakeUp) backCtx.drawImage(snake.image,snake.x-5,snake.y,20,20);
+  if(snake.image == snakeDown) backCtx.drawImage(snake.image,snake.x-5,snake.y,20,20);
+  if(snake.image == snakeLeft) backCtx.drawImage(snake.image,snake.x,snake.y-5,20,20);
+  if(snake.image == snakeRight) backCtx.drawImage(snake.image,snake.x,snake.y-5,20,20);
+  
+  //Display scores
+  backCtx.font = "15px Segoe UI";
+  backCtx.fillStyle = "black";
+  backCtx.fillText("Score: " + score, 10, 450);
+  backCtx.fillText("Highscore: " + highScore, 10, 470);
 }
 
+//Resets all input to false
 function resetInput() {
   input.up = false;
   input.down = false;
@@ -189,11 +269,34 @@ function resetInput() {
   input.right = false;
 }
 
+//Resets all previous input to false
 function resetPrevInput() {
   prevInput.up = false;
   prevInput.down = false;
   prevInput.left = false;
   prevInput.right = false;
+}
+
+//Spawns an apple in a random location. Makes sure it won't collide with the snake or the walls
+function spawnApple() {
+  newX = Math.floor(Math.random()*backBuffer.width);
+  newY = Math.floor(Math.random()*backBuffer.height);
+  for(i=0; i<snakeX.length; i++) {
+    if(!(newX < snakeX[i] + snake.width &&
+      newX + apple.width > snakeX[i] &&
+      newY < snakeY[i] + snake.height &&
+      newY + apple.height > snakeY[i]) && 
+      (newX + apple.width < backBuffer.width &&
+       newX > apple.width &&
+       newY + apple.height < backBuffer.height &&
+       newY > apple.height)) {
+        apple.x = newX;
+        apple.y = newY;
+      }
+    else {
+      spawnApple();
+    }
+  }
 }
 
 /* Launch the game */
